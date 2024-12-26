81721b3c84b2bab0164beff63949d347a6868f14
Ilya Tiron <tironcoach@gmail.com>
2024-12-26 14:31:49 +0300

    pages folder commit

---
+++ pages/base_page.py
@@ -0,0 +1,13 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+class BasePage():
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        self.browser.get(self.url)
