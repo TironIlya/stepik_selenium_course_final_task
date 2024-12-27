2d1aa2c76c7446b0e2accccce05573430e6ee9c5
Ilya Tiron <tironcoach@gmail.com>
2024-12-27 10:27:21 +0300

    update commit

--- test_main_page.py
+++ test_main_page.py
@@ -1,20 +1,11 @@
-import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from .pages.main_page import MainPage
 from selenium.webdriver.common.by import By
 
 
-@pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    service = Service(ChromeDriverManager().install())  # Создание объекта Service с путем к драйверу
-    browser = webdriver.Chrome(service=service)  # Передача сервиса в webdriver
-    yield browser
-    browser.quit()
-
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page = MainPage(
+        browser, link
+    )  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
