b39951ab5b965df2fbb156e8b0ffd16fc6e4e382
Ilya Tiron <tironcoach@gmail.com>
2024-12-28 17:48:38 +0300

    NoSuchElement commit

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 class BasePage():
     def __init__(self, browser, url):
@@ -11,3 +12,18 @@ class BasePage():
 
     def open(self):
         self.browser.get(self.url)
+    
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
+    
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
