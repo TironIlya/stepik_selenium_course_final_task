5f1d178db0160c7f7fde0a54b208667eca734aa3
Ilya Tiron <tironcoach@gmail.com>
2025-01-20 11:02:46 +0300

    basket page tests commit

--- pages/product_page.py
+++ pages/product_page.py
@@ -2,8 +2,6 @@ from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
 
 
-
-
 class ProductPage(BasePage):
     # Локаторы
     ADD_TO_BASKET_BUTTON = (By.CLASS_NAME, "btn-add-to-basket")
--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -1,38 +1,26 @@
 import pytest
 from pages.product_page import ProductPage
+from pages.basket_page import BasketPage
 
 
 @pytest.mark.parametrize("link", ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"])
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser, link):
-    # Открываем страницу товара
     page = ProductPage(browser, link)
     page.open()
-
-    # Добавляем товар в корзину
     page.add_product_to_basket()
-
-    # Проверяем, что нет сообщения об успехе
     page.should_not_be_success_message()
 
 @pytest.mark.parametrize("link", ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"])
 def test_guest_cant_see_success_message(browser, link):
-    # Открываем страницу товара
     page = ProductPage(browser, link)
     page.open()
-
-    # Проверяем, что нет сообщения об успехе
     page.should_not_be_success_message()
 
 @pytest.mark.parametrize("link", ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"])
 def test_message_disappeared_after_adding_product_to_basket(browser, link):
-    # Открываем страницу товара
     page = ProductPage(browser, link)
     page.open()
-
-    # Добавляем товар в корзину
     page.add_product_to_basket()
-
-    # Проверяем, что сообщение об успехе исчезает
     page.should_disappear_success_message()
 
 def test_guest_should_see_login_link_on_product_page(browser):
@@ -41,8 +29,17 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
-def test_guest_can_go_to_login_page_from_product_page (browser):
+def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_empty_basket()
+    basket_page.should_be_empty_basket_text()
